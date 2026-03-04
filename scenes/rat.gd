7cdb9bca1cad2dbb99d943d4df91a62892b1a1bf
LitMit-dev <lhtmlburner@gmail.com>
2026-03-04 22:34:08 +0000

    Started speak mechanic

--- scenes/rat.gd
+++ scenes/rat.gd
@@ -5,6 +5,34 @@ const SPEED = 340.0
 const jump_speed = -590.0
 var jumpAvailible = true
 
+var isSinging = false
+
+var text = "_____"
+
+func update_voice():
+	$LineEdit.text = $LineEdit.text.remove_chars(" ")
+	$LineEdit.caret_column = 5
+	for i in range(5):
+		if i >= len($LineEdit.text):
+			text[i] = "_"
+		else:
+			text[i] = $LineEdit.text[i]
+		get_node("LETTERBOX/" + str(i)).text = text[i]
+	
+
+func start_voice():
+	$LineEdit.grab_focus()
+	$"text anim".play("LETTERS")
+	$"speak anim".play("drop")
+	await get_tree().create_timer(0.4).timeout
+	isSinging = true
+	
+func end_voice():
+	$LineEdit.release_focus()
+	$"speak anim".play_backwards("drop")
+	await get_tree().create_timer(0.4).timeout
+	isSinging = false
+
 func jump():
 	velocity.y = jump_speed
 	$Rat_Sprite.frame = 1
@@ -13,11 +41,25 @@ func jump():
 func CoyoteEnd():
 	jumpAvailible = false
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("START_SONG") and !isSinging:
+			start_voice()
+		elif event.is_action_pressed("END_SONG") and isSinging:
+			end_voice()
+			
+func _process(delta: float) -> void:
+	if isSinging:
+		$LineEdit.grab_focus()
+		update_voice()
+		for i in [0,1,2,3,4]:
+			if i == $LineEdit.caret_column:
+				get_node("LETTERBOX/" + str(i)).modulate = Color(1.0, 1.0, 0.0, 1.0)
+			else:
+				get_node("LETTERBOX/" + str(i)).modulate = Color(1.0, 1.0, 1.0, 1.0)
+	
+
 func _physics_process(delta):
-	#finish off tmr
-	col = ($floorbelow2.is_colliding() and $floorbelow3.is_colliding())
-	get_node("collider0").disabled = col
-	get_node("collider1").disabled = !col
 	
 	if velocity.y <= 0 and !jumpAvailible and is_on_floor():
 		jumpAvailible = true
@@ -30,16 +72,16 @@ func _physics_process(delta):
 
 	velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and jumpAvailible:
+	if Input.is_action_just_pressed("ui_accept") and jumpAvailible and !isSinging:
 		jump()
 		
 	if (!is_on_floor()) and jumpAvailible and $CoyoteTimer.is_stopped():
 		$CoyoteTimer.start()
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and !isSinging:
 		velocity.x = direction * SPEED
 		$Rat_Sprite.flip_h = velocity.x > 0
 		if get_real_velocity() == Vector2(0,0):
@@ -52,3 +94,17 @@ func _physics_process(delta):
 			$Rat_Anims.play("idle")
 
 	move_and_slide()
+
+
+func check_text(new_text: String) -> void:
+	#var caret = $LineEdit.caret_column
+	
+	$LineEdit.text = new_text.remove_chars("E")
+	$LineEdit.text = new_text.remove_chars("e")
+	
+	
+
+
+func text_submit(new_text: String) -> void:
+	$LineEdit.grab_focus()
+	#fix
